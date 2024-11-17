8308e7ef366c55aa1ad8d2a401dfbf55d95d2ab2
Lol <L@ol>
2024-11-17 12:00:30 +0200

    evrth is done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    $('.design-slider').slick({
+        dots: false,
+        slidesToShow: 1,
+        variableWidth: true,
+        prevArrow: '<img src="images/arrow-left.svg" alt="" class="arrow arrow-left slick-prev">',
+        nextArrow: '<img src="images/arrow-right.svg" alt="" class="arrow arrow-right slick-next">'
+    })
 });
